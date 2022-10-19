e92b89d56c5b5e843c018d3fe5ac17982c54576b
Peter Debevec <peterusdebevec@gmail.com>
2022-10-19 14:59:45 +0200

    ne vem

--- KompleksnaŠT/KompleksnaŠT/Form1.cs
+++ KompleksnaŠT/KompleksnaŠT/Form1.cs
@@ -24,6 +24,7 @@ namespace KompleksnaŠT
             k1.Re = Convert.ToDouble(tb11.Text); k1.Im = Convert.ToDouble(tb12.Text);
             k2.Re = Convert.ToDouble(tb11.Text); k2.Im = Convert.ToDouble(tb12.Text);
         }
+        
         private void b1_Click(object sender, EventArgs e)
         {
             NovaKompleksnašt();
