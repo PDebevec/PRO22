89c1f31f653727e3e66be0526bcb6da4f7ef08de
Peter Debevec <peter.debevec@scng.si>
2022-10-19 08:26:49 +0200

    operator overloading

--- Strukture/Strukture/Program.cs
+++ Strukture/Strukture/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,34 @@ namespace Strukture
         {
             public double x, y, z;
             public override string ToString()
+                => "(" + x + "," + y + "," + z + ")";
+            
+            //ne moremo itemti kostruktorja brez parametrov
+            public Vektor(double x1, double y1, double z1)
             {
-                return "(" + x + "," + y + "," + z + ")";
+                x = x1;y = y1;z = z1;
             }
+            public Vektor(Vektor v)
+            {
+                x = v.x;
+                y = v.y;
+                z = v.z;
+            }
+            public static Vektor operator+ (Vektor v1, Vektor v2)
+            {
+                return new Vektor(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+            }
+            //taku je lepše
+            public static Vektor operator* (double a, Vektor v)
+                => new Vektor(v.x * a, v.y * a, v.z * 5);
+            public static Vektor operator* (Vektor v, double a)
+                => a * v;
+            public static double operator* (Vektor a, Vektor b)
+                => a.x * b.x + a.y * b.y + a.z * b.z;
+            public static bool operator ==(Vektor a, Vektor b)
+                => a.x == b.x && a.y == b.y && a.z == b.z;
+            public static bool operator !=(Vektor a, Vektor b)
+                => a.x != b.x && a.y != b.y && a.z != b.z;
         }
         static void Main(string[] args)
         {
@@ -22,12 +49,12 @@ namespace Strukture
             v.x = 1;
             v.y = -1;
             v.z = 0;
-            Vektor a = new Vektor();
-            a.x = 2;
-            a.x = 2;
-            a.z = 3;
+            Vektor a = new Vektor(2, 2, 3);
+
             Console.WriteLine(v.ToString());
             Console.WriteLine(a.ToString());
+            Vektor c = 5 * v + a;
+            Console.WriteLine(c.ToString());
             Console.ReadLine();
         }
     }
