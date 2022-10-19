dde505db08c20c3a6a8b7a3fa77cf387714a8f7d
Peter Debevec <peter.debevec@scng.si>
2022-10-19 08:42:58 +0200

    can I get uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

---
+++ Pravokotnik/Pravokotnik/Pravokotnik.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pravokotnik
+{
+    internal class Pravokotnik
+    {
+        public Pravokotnik() { višina = 0; širina = 0; }
+        public Pravokotnik(double a, double b)
+        {
+            višina = a;
+            širina = b;
+        }
+        
+        private double višina;
+        private double širina;
+        public double Višina { get => višina; set => višina = value; }
+        public double Širina { get => širina; set => širina = value; }
+        
+        private static double Površina(double a, double b)
+            => a * b;
+        public static bool operator >(Pravokotnik a, Pravokotnik b)
+            => Površina(a.višina, a.širina) > Površina(b.višina, b.širina);
+        public static bool operator <(Pravokotnik a, Pravokotnik b)
+            => Površina(a.višina, a.širina) < Površina(b.višina, b.širina);
+        public static bool operator >=(Pravokotnik a, Pravokotnik b)
+            => Površina(a.višina, a.širina) >= Površina(b.višina, b.širina);
+        public static bool operator <=(Pravokotnik a, Pravokotnik b)
+            => Površina(a.višina, a.širina) <= Površina(b.višina, b.širina);
+        public static bool operator ==(Pravokotnik a, Pravokotnik b)
+            => a.višina == b.višina && a.širina == b.širina;
+        public static bool operator !=(Pravokotnik a, Pravokotnik b)
+            => a.višina != b.višina && a.širina != b.širina;
+        
+        public override string ToString()
+            => "Višina = " + višina + "\t" + "Širina " + širina;
+    }
+}
