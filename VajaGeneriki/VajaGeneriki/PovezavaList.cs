6a608bbd57861e934cedeff18438bdddfcd5c810
Peter Debevec <peter.debevec@scng.si>
2022-10-26 08:27:24 +0200

    kr neki

---
+++ VajaGeneriki/VajaGeneriki/PovezavaList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VajaGeneriki
+{
+    internal class PovezavaList<K,T> where K:IComparable
+    {
+        Vozel<K, T> glava;
+        public PovezavaList()
+        {
+            glava = new Vozel<K, T>();
+        }
+        public void Dodaj(K k, T t)
+        {
+            Vozel<K, T> nov = new Vozel<K, T>(k, t, glava.naslednji);
+            glava.naslednji = nov;
+        }
+        public T Najdi(K k)
+        {
+            Vozel<K, T> trenutni = glava;
+            while(trenutni.naslednji != null)
+            {
+                if (trenutni.ključ.CompareTo(k)==0)
+                    break;
+                else
+                    trenutni = trenutni.naslednji;
+            }
+            return trenutni.vsebina;
+        }
+    }
+}
---
+++ VajaGeneriki/VajaGeneriki/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VajaGeneriki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PovezavaList<int, string> x = new PovezavaList<int, string>();
+            x.Dodaj(1, "lalala");
+            x.Dodaj(5, "cccc");
+            x.Dodaj(2, "mijav");
+            string r = x.Najdi(5);
+            Console.WriteLine(r);
+            Console.ReadLine();
+        }
+    }
+}
