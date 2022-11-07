8a8c33da2d031183112a828c18c661c205ddfa64
Peter Debevec <peter.debevec@scng.si>
2022-11-07 08:54:31 +0100

    ne vem

---
+++ KličiMe/KličiMe/Bralec.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace KličiMe
+{
+    internal class Bralec
+    {
+        FileStream fs;
+        StreamReader sr; //iz katere datoteke
+        uint st; //koliko oseb naj kličem
+
+        public void Odpri(string fileLocation)
+        {
+            fs = new FileStream(fileLocation, FileMode.Open);
+            sr = new StreamReader(fs);
+            try
+            {
+                string prvaVrstica = sr.ReadLine();
+                st = uint.Parse(prvaVrstica);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Prvo število v datoteki ni število oz. pozitivno");
+            }
+        }
+        public uint nOseb { get { return st; } } //getter metoda za splemenljivko st
+
+        public void obravnavajNaslednjega()
+        {
+            try
+            {
+                string ime = sr.ReadLine();
+                if (ime == null)
+                {
+                    throw new KličiMeException("Ni dovol imen");
+                }
+                if (ime[0] == 'Z')
+                {
+                    throw new TajniAgentException(ime);
+                }
+                Console.WriteLine("Pokliči " + ime);
+            }
+            catch (TajniAgentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ KličiMe/KličiMe/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace KličiMe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string imedatoteke = "C:\\D disk\\git\\PRO22\\KličiMe\\KličiMe\\klici.txt";
+            Bralec bralec = new Bralec();
+            try
+            {
+                bralec.Odpri(imedatoteke);
+                for (int i = 0; i < bralec.nOseb; i++)
+                {
+                    bralec.obravnavajNaslednjega();
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("datoteka " + imedatoteke + " ne obstaja");
+            }
+            catch (KličiMeException k)
+            {
+                Console.WriteLine("datoteka ima napačno strukturo");
+                Console.WriteLine(k.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("neznana napaka " + e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
