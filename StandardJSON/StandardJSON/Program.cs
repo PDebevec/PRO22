868c26fb7e129ad8da7f978cce687c822695bf8a
Peter Debevec <peter.debevec@scng.si>
2022-11-10 11:00:47 +0100

    json pog

--- StandardJSON/StandardJSON/Program.cs
+++ StandardJSON/StandardJSON/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 
 namespace StandardJSON
 {
@@ -10,6 +13,22 @@ namespace StandardJSON
     {
         static void Main(string[] args)
         {
+            //NogometnaLiga n = new NogometnaLiga();
+            //string oblikaZapisa = JsonConvert.SerializeObject(n, Formatting.Indented);
+            //FileStream fs = new FileStream("liga.json", FileMode.Create);
+            //StreamWriter sw = new StreamWriter(fs);
+            //sw.Write(oblikaZapisa);
+            //sw.Close();
+
+            FileStream fs = new FileStream("liga.json", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            string prebrano = sr.ReadToEnd();
+            NogometnaLiga n = (NogometnaLiga)JsonConvert.DeserializeObject<NogometnaLiga>(prebrano);
+            foreach (Ekipa i in n.liga)
+            {
+                Console.WriteLine(i.ime);
+            }
+            Console.ReadLine();
         }
     }
 }
