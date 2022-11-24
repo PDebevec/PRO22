20a29e61bf6d28b9e8335f979350fc756957e27e
Peter Debevec <peter.debevec@scng.si>
2022-11-24 10:57:43 +0100

    sql

---
+++ ŠtudentWF/ŠtudentWF/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ŠtudentWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbpredmet = new System.Windows.Forms.TextBox();
+            this.tbstudent = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lrezultat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Predmet";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "študent";
+            // 
+            // tbpredmet
+            // 
+            this.tbpredmet.Location = new System.Drawing.Point(81, 13);
+            this.tbpredmet.Name = "tbpredmet";
+            this.tbpredmet.Size = new System.Drawing.Size(100, 20);
+            this.tbpredmet.TabIndex = 2;
+            // 
+            // tbstudent
+            // 
+            this.tbstudent.Location = new System.Drawing.Point(81, 67);
+            this.tbstudent.Name = "tbstudent";
+            this.tbstudent.Size = new System.Drawing.Size(100, 20);
+            this.tbstudent.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(242, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "izpiši oceno";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Izpis_ocene);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(242, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "izpiši povprečje";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Izpis_povprecje);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "prikaži vse predmete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Izpis_predmetov);
+            // 
+            // lrezultat
+            // 
+            this.lrezultat.AutoSize = true;
+            this.lrezultat.Location = new System.Drawing.Point(13, 188);
+            this.lrezultat.Name = "lrezultat";
+            this.lrezultat.Size = new System.Drawing.Size(41, 13);
+            this.lrezultat.TabIndex = 7;
+            this.lrezultat.Text = "rezultat";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 452);
+            this.Controls.Add(this.lrezultat);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbstudent);
+            this.Controls.Add(this.tbpredmet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbpredmet;
+        private System.Windows.Forms.TextBox tbstudent;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lrezultat;
+    }
+}
+
---
+++ ŠtudentWF/ŠtudentWF/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ŠtudentWF
+{
+    public partial class Form1 : Form
+    {
+        string povezava = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\D disk\git\baza\študent\ŠtudentNet.mdf"";Integrated Security=True;Connect Timeout=30";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Izpis_ocene(object sender, EventArgs e)
+        {
+            lrezultat.Text = "rezultat ";
+            SqlConnection p = new SqlConnection(povezava);
+            string ukaz = "select s.imeštudenta, o.ocena from ocene o join predmet p on o.idpredmeta = p.idpredmeta join študent s on o.idštudenta = s.idvpisa where p.imepredmeta = '" + tbpredmet.Text + "'";
+            SqlCommand u = new SqlCommand();
+            u.Connection = p;
+            u.CommandText = ukaz;
+            u.CommandType = CommandType.Text;
+            p.Open();
+            SqlDataReader bralec = u.ExecuteReader();
+            while (bralec.Read())
+            {
+                lrezultat.Text += Environment.NewLine + bralec[0] + bralec[1];
+            }
+            bralec.Close();
+            p.Close();
+        }
+
+        private void Izpis_povprecje(object sender, EventArgs e)
+        {
+            lrezultat.Text = "rezultat ";
+            SqlConnection p = new SqlConnection(povezava);
+            SqlCommand u = new SqlCommand();
+            u.Connection = p;
+            p.Open();
+            string avg = "select avg(o.ocena) from študent s join ocene o on s.idvpisa = o.idštudenta where SUBSTRING(s.imeštudenta, CHARINDEX(' ', s.imeštudenta) + 1, LEN(s.imeštudenta) - CHARINDEX(' ', s.imeštudenta)) = '" + tbstudent.Text + "'";
+            u.CommandText = avg;
+            SqlDataReader bralec = u.ExecuteReader();
+            u.CommandText = "update študent set povprečnaocena = ( " + avg + " )";
+            u.CommandType = CommandType.Text;
+            bralec.Close();
+            u.ExecuteNonQuery();
+            u.CommandText = "select s.imeštudenta, s.povprečnaocena from študent s where SUBSTRING(s.imeštudenta, CHARINDEX(' ', s.imeštudenta) + 1, LEN(s.imeštudenta) - CHARINDEX(' ', s.imeštudenta)) = '" + tbstudent.Text + "'";
+            bralec.Close();
+            bralec = u.ExecuteReader();
+            while (bralec.Read())
+            {
+                lrezultat.Text += Environment.NewLine + bralec[0] + bralec[1];
+            }
+            bralec.Close();
+            p.Close();
+        }
+
+        private void Izpis_predmetov(object sender, EventArgs e)
+        {
+            lrezultat.Text = "rezultat ";
+            SqlConnection p = new SqlConnection(povezava);
+            string ukaz = "select * from predmet";
+            SqlCommand u = new SqlCommand();
+            u.Connection = p;
+            u.CommandText = ukaz;
+            u.CommandType = CommandType.Text;
+            p.Open();
+            SqlDataReader bralec = u.ExecuteReader();
+
+            bralec.Close();
+            p.Close();
+        }
+    }
+}
