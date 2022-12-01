e6c9086546524864af416332f65ed1d205025a29
Peter Debevec <peter.debevec@scng.si>
2022-12-01 10:58:39 +0100

    lambda

--- UvodDelegati/UvodDelegati/Program.cs
+++ UvodDelegati/UvodDelegati/Program.cs
@@ -8,8 +8,22 @@ namespace UvodDelegati
 {
     internal class Program
     {
+        public delegate bool funkcijaZaNize(string s);
         static void Main(string[] args)
         {
+            string[] imena = { "Adam", "Alan", "Bob", "Seven", "Jim", "Aiden", "Rob", "bill", "Jacob", "James" };
+            
+            Console.ReadLine();
+        }
+        public static List<string> DelajOperacijeNaNizi(string[] mojinizi, funkcijaZaNize mojafunkcija)
+        {
+            List<string> nova = new List<string>();
+            foreach (var i in mojinizi)
+            {
+                if (mojafunkcija(i))
+                    nova.Add(i);
+            }
+            return nova;
         }
     }
 }
